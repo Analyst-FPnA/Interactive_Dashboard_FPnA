834953f894fbb4e34873af089daa86739b67919d
Analyst-FPnA <analyst.fpna@gmail.com>
2024-11-21 11:27:31 +0700

    Update Dashboard

--- interactive.py
+++ interactive.py
@@ -6,6 +6,9 @@ st.title("Interactive Dashboard FPnA")
 # Deskripsi
 st.markdown("### Klik pada kotak di bawah untuk menuju ke halaman tertentu!")
 
+# Deskripsi
+st.markdown("DASHBOARD")
+
 # Layout kotak interaktif
 col1, col2= st.columns(2)
 
@@ -44,6 +47,27 @@ with col4:
         "<a href='https://dashboard-inventaris.streamlit.app/' target='_blank' style='text-decoration:none;'>"
         "<div style='background-color:#800000;padding:20px;text-align:center;border-radius:10px;color:black; font-family:Roboto,sans-serif; font-size: 16px;'>"
         "<strong style='color:white;'>INVENTORY CONTROL</strong></div>"
+        "</a>",
+        unsafe_allow_html=True,
+    )
+
+# Baris ketiga
+col5, col6 = st.columns(2)
+
+with col5:
+    st.markdown(
+        "<a href='https://https://dashboard-selisih-ojol.streamlit.app/' target='_blank' style='text-decoration:none;'>"
+        "<div style='background-color:#800000;padding:20px;text-align:center;border-radius:10px;color:black; font-family:Roboto,sans-serif; font-size: 16px;'>"
+        "<strong style='color:white;'>REKAP SELISIH OJOL</strong></div>"
+        "</a>",
+        unsafe_allow_html=True,
+    )
+
+with col6:
+    st.markdown(
+        "<a href='https://dashboard-promix.streamlit.app/' target='_blank' style='text-decoration:none;'>"
+        "<div style='background-color:#800000;padding:20px;text-align:center;border-radius:10px;color:black; font-family:Roboto,sans-serif; font-size: 16px;'>"
+        "<strong style='color:white;'>PRODUCT MIX</strong></div>"
         "</a>",
         unsafe_allow_html=True,
     )
